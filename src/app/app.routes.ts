f72705915b80204458f08eb91894ed16d7479ed6
Patroklos <enzo-renzo@hotmail.com>
2024-01-08 07:36:04 -0500

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import {LoginPageComponent} from "./login/login-page/login-page.component";
 import {LoginFormComponent} from "./login/login-form/login-form.component";
 
 export const routes: Routes = [
+  {"path" : "", component:LoginPageComponent}
 ];
